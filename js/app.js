59668837890d0f32ced24b03d90efbc699808509
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-04-13 02:45:58 -0400

    Add if statements to js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
-    $("#fizz-buzz-form").submit(function (event) {
-    	event.preventDefault()
+    $("#fizz-buzz").submit(function (event) {
+        event.preventDefault()
         
-        var fizzBuzz = $("#integer").val();
+        var fizzBuzz = $('#user-input').val();
 
-        if ((fizzBuzz % 3) == 0) {
-            .alert("hi!");
-        }
+        if (fizzBuzz % 3 === 0) {
+            $('#list').append('<li>FIZZ</li>');
+            $('#user-input').val("");
+        } if (fizzBuzz % 5 === 0) {
+            $('#list').append('<li>BUZZ</li>');
+            $('#user-input').val("");
+        } if (fizzBuzz % 5 != 0, fizzBuzz % 3 != 0) {
+            $('#list').append('<li>' + fizzBuzz + '</li>');
+            $('#user-input').val("");
+        } if (fizzBuzz % 5 === 0, fizzBuzz % 3 === 0) {
+            $('#list').append('<li>FIZZ BUZZ!</li>');
+            $('#user-input').val("");
+        }   
+        
     });
+
 });
+
+
+// Prepend li from list
+
